e3cbe1fe673503b83762775f679ba3deec1d4ce3
Pooja Chinchansur <poojapc027@gmail.com>
2025-07-14 21:53:02 +0530

    Add base path to fix Netlify blank screen

---
+++ client/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>🚀 Hello from Pooja’s React App!</h1>
+      <p>Your frontend is successfully deployed on Netlify.</p>
+    </div>
+  );
+}
+
+export default App;
